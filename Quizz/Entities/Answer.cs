72c9027c369a9633fed4c47bab6f6fe0f1974d37
hoby <hobyfln@gmail.com>
2019-09-24 15:20:27 +0200

    Push

---
+++ Quizz/Entities/Answer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Entities
+{
+    public partial class Answer
+    {
+        #region Fields
+            //[ForeignKey(Answer)]
+            private int _idAnswer;
+            private string _idQuestion;
+            private string _type;
+
+        #endregion
+
+        #region Getters/Setters
+
+        public int IdAnswer { get => _idAnswer; set => IdAnswer = value; }
+        public string IdQUestion { get => _idQuestion; set => IdQUestion= value ; }
+        public string Type { get => _idQuestion; set => Type= value ; }
+
+        #endregion;
+    }
+}
---
+++ Quizz/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Entities
+{
+    public partial class User
+    {
+
+        public int userId { get; set; }
+        public int idQuizz { get; set; }
+        public bool isAdmin { get; set; }
+
+    }
+}
